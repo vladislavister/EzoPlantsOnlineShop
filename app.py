40db41d1300466610f3e4416740e314a205e1cf3
vladislavister2 <vladislavisterr@gmail.com>
2022-11-20 16:50:37 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
